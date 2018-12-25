1d5f8bf76fac11667a78cdb30d41d1c1d7dcec0e
Paul Schaefer <yeppaul2@gmail.com>
2018-12-25 14:03:53 -0500

    Added movies and simple HAML loop over the movies

--- app.rb
+++ app.rb
@@ -9,5 +9,12 @@ config_file 'data/config.yml'
 
 # On '/' page, do this...
 get '/' do
+  # Open movie file
+  File.open('data/movies.json') do |f|
+    # Return JSON to the page
+    @movies = JSON.parse(f.read)
+  end
+
+  # Render views/index.haml
   haml :index
 end
