13762f8bc092ea1024d228fdf75a28941948e1a9
Paul Schaefer <yeppaul2@gmail.com>
2018-12-27 14:48:04 -0500

    added additional path to Sprockets environment to hopefully fix broken Heroku deployment

--- lib/assets.rb
+++ lib/assets.rb
@@ -4,6 +4,7 @@ module Assets
     environment = Sprockets::Environment.new root_path
     environment.append_path 'assets'
     environment.append_path 'vendor/bundle/gems/'
+    environment.append_path 'vendor/bundle/ruby/2.4.0/gems'
     environment
   end
 end
