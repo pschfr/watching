ea8d0d2a3614ea37f1273c353ac0869f950a3958
Paul Schaefer <yeppaul2@gmail.com>
2018-12-30 12:00:10 -0500

    Simplified /search and /add path logic

--- app.rb
+++ app.rb
@@ -30,36 +30,27 @@ get '/' do
   haml :index
 end
 
-# On '/search?movie=title', do this...
-get '/search' do
+# On '/search?movie=title' or '/add?movie=title', do this...
+get ['/search', '/add'] do
   # Get movie title from query parameters
   query = params['movie']
 
   # Return result to page
   @result = TMDb::Movie.search(query).first
 
-  # Render views/result.haml
-  haml :result
-end
+  # If on '/add', actually add the poster path to the JSON file
+  if request.path_info == '/add'
+    # Builds temporary hashes for storing the images
+    poster_hash   = { @result.title => "#{IMAGE_PATH}#{@result.poster_path}" }
 
-# On '/add?movie=title', do this...
-get '/add' do
-  # Get movie title from query parameters
-  query = params['movie']
+    # Opens JSON file for reading
+    poster_json   = JSON[File.read(POSTER_PATH)]
+    poster_json   = [poster_json] if poster_json.class != Array
 
-  # Return result to page
-  @result = TMDb::Movie.search(query).first
-
-  # Builds temporary hashes for storing the images
-  poster_hash   = { @result.title => "#{IMAGE_PATH}#{@result.poster_path}" }
-
-  # Opens JSON file for reading
-  poster_json   = JSON[File.read(POSTER_PATH)]
-  poster_json   = [poster_json] if poster_json.class != Array
-
-  # Append hash to JSON file
-  File.open(POSTER_PATH, 'w') do |f|
-    f.write(JSON.pretty_generate(poster_json << poster_hash))
+    # Append hash to JSON file
+    File.open(POSTER_PATH, 'w') do |f|
+      f.write(JSON.pretty_generate(poster_json << poster_hash))
+    end
   end
 
   # Render views/result.haml
